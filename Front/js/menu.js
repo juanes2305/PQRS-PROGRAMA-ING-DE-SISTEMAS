f8715f5f420633dd6acea5fdf32a2fc758e22fc0
yeinerd06 <capitan.yeiner@gmail.com>
2023-05-16 06:53:44 -0500

    se realiza la plantilla front-end

---
+++ Front/js/menu.js
@@ -0,0 +1,11 @@
+
+
+function openNav() {
+    document.getElementById("sideNavigation").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+}
+function closeNav() {
+    document.getElementById("sideNavigation").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+}
+
